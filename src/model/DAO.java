61293a6d4ca744a0b16b5d6c104c5264c345cbb1
Jorian <jorian@outlook.com>
2017-09-27 17:17:32 +0200

    Added DAO file

---
+++ src/model/DAO.java
@@ -0,0 +1,18 @@
+package model;
+
+public class DAO {
+
+    /*
+    "Controller should be only responsible for changing the state of model layer.
+    DAOs are part of persistence mechanism." - mefisto, stackexchange.com
+
+    This class handles the persistence (storage) of models to a file on the hard drive.
+    JSON is used here. JAAAAAASOOOOOOOOOOOOOON!!!
+
+     */
+
+    public void writeToFile() {
+
+    }
+
+}
