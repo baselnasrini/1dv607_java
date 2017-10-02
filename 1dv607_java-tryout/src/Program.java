7d246fab01074c8e5a83c726304fe5af89a37a88
baselnasrini <beso_o@live.com>
2017-10-02 22:33:38 +0200

    Delete Program.java

--- 1dv607_java-tryout/src/Program.java
+++
@@ -1,9 +0,0 @@
-import controller.ViewController;
-
-public class Program {
-
-    public static void main(String[] args) {
-        ViewController vC = new ViewController();
-        vC.startProgram();
-    }
-}
